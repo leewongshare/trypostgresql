2fd45d8cae7f1ee73c3616cf5dea127322356d13
wong lee <write.wonglee@gmail.com>
2012-10-22 12:12:39 -0400

    try postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trypostgresql::Application.config.secret_token = 'b1c1ded60a3de1822b94986928233f8bc5a0973a3ab7975a5761ee8783904f06c4e7a772eea945d4fef80e0328809ccdcae47f27a09302507abc192c1b74ebe9'
